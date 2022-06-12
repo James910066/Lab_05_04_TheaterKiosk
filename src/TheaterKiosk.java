d3de42289a70a2d0abdd853c434bc35a39ea7049
James910066 <james.dinser@gmail.com>
2022-06-12 11:59:00 -0400

    Added lab05 04 Theater Kiosk

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,23 @@
+public class TheaterKiosk
+
+{
+    public static void main(String[] args)
+    {
+        //num AGE_LIMIT = 21
+        //num age = 0
+        //output "Enter your age: "
+        //input age
+        //If age >= AGE_LIMIT then
+        //output "You get a wrist band."
+        //endIf
+
+
+        final int AGE_LIMIT = 21;
+        int age = 21;
+        if (age >= AGE_LIMIT)
+        {
+            System.out.println("You get a wristband");
+        }
+
+    }
+}
